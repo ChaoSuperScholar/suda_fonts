57c32b77c0dacfbd022221aab3f064cb4c0b1334
sc_track <sc_track@163.com>
2018-12-22 18:26:16 +0800

    申诉页面路由冲突后再次添加

--- src/router/index.js
+++ src/router/index.js
@@ -38,197 +38,201 @@ import transferRecord from '../page/transferRecord'
 import index from '../page/index'
 import createWallet from '../page/createWallet'
 import importWallet from '../page/importWallet'
-
+import appeal from '../page/appeal'
 
 Vue.use(Router)
 
 export default new Router({
-  /*mode:'history',*/
-  routes: [
-  	{
-			path: '/',
-			name: 'start',
-			component: start
-		},
-		{
-			path: '/indexNew',
-			name: 'indexNew',
-			component: indexNew
-		},
-		{
-			path: '/registered',
-			name: 'registered',
-			component: registered
-		},
-		{
-			path: '/receivables',
-			name: 'receivables',
-			component: receivables
-		},
-		{
-			path: '/addAssets',
-			name: 'addAssets',
-			component: addAssets
-		},
-		{
-			path: '/wallet',
-			name: 'wallet',
-			component: wallet
-		},
-		{
-			path: '/transaction',
-			name: 'transaction',
-			component: transaction
-		},
-    {
-      path: '/index',
-      name: 'index',
-      component: index
-    },
-		{
-			path: '/createWallet',
-			name: 'createWallet',
-			component: createWallet
-		},
-		{
-			path: '/importWallet',
-			name: 'importWallet',
-			component: importWallet
-		},
-    {
-    	path: '/components',
-      name: 'header',
-      component: header
-    },
-		{
-			path: '/components',
-			name: 'footerBar',
-			component: footerBar
-		},
-		{
-			path: '/components',
-			name: 'pageHead',
-			component: pageHead
-		},
-		{
-			path: '/information',
-			name: 'information',
-			component: information
-		},
-		{
-			path: '/application',
-			name: 'application',
-			component: application
-		},
-		{
-			path: '/userCenter',
-			name: 'userCenter',
-			component: userCenter
-		},
-		{
-			path: '/transactionRelease',
-			name: 'transactionRelease',
-			component: transactionRelease
-		},
-		{
-			path: '/order',
-			name: 'order',
-			component: order
-		},
-		{
-			path: '/orderPage',
-			name: 'orderPage',
-			component: orderPage
-		},
-		{
-			path: '/forgetPassword',
-			name: 'forgetPassword',
-			component: forgetPassword
-		},
-		{
-			path: '/securityCenter',
-			name: 'securityCenter',
-			component: securityCenter
-		},
-		{
-			path: '/moreInformation',
-			name: 'moreInformation',
-			component: moreInformation
-		},
-		{
-			path: '/aboutUs',
-			name: 'aboutUs',
-			component: aboutUs
-		},
-		{
-			path: '/myTeam',
-			name: 'myTeam',
-			component: myTeam
-		},
-		{
-			path: '/personalData',
-			name: 'personalData',
-			component: personalData
-		},
-		{
-			path: '/financial',
-			name: 'financial',
-			component: financial
-		},
-		{
-			path: '/myInvestment',
-			name: 'myInvestment',
-			component: myInvestment
-		},
-		{
-			path: '/tenpay',
-			name: 'tenpay',
-			component: tenpay
-		},
-		{
-			path: '/walletDetails',
-			name: 'walletDetails',
-			component: walletDetails
-		},
-		{
-			path: '/walletTransfer',
-			name: 'walletTransfer',
-			component: walletTransfer
-		},
-		{
-			path: '/contacts',
-			name: 'contacts',
-			component: contacts
-		},
-		{
-			path: '/modifyContact',
-			name: 'modifyContact',
-			component: modifyContact
-		},
-		{
-			path: '/addContact',
-			name: 'addContact',
-			component: addContact
-		},
-		{
-			path: '/identity',
-			name: 'identity',
-			component: identity
-		},
-		{
-			path: '/paymentMethod',
-			name: 'paymentMethod',
-			component: paymentMethod
-		},
-		{
-			path: '/addPayment',
-			name: 'addPayment',
-			component: addPayment
-		},
-		{
-			path: '/transferRecord',
-			name: 'transferRecord',
-			component: transferRecord
-		}
-  ]
+    /*mode:'history',*/
+    routes: [{
+            path: '/',
+            name: 'start',
+            component: start
+        },
+        {
+            path: '/indexNew',
+            name: 'indexNew',
+            component: indexNew
+        },
+        {
+            path: '/registered',
+            name: 'registered',
+            component: registered
+        },
+        {
+            path: '/receivables',
+            name: 'receivables',
+            component: receivables
+        },
+        {
+            path: '/addAssets',
+            name: 'addAssets',
+            component: addAssets
+        },
+        {
+            path: '/wallet',
+            name: 'wallet',
+            component: wallet
+        },
+        {
+            path: '/transaction',
+            name: 'transaction',
+            component: transaction
+        },
+        {
+            path: '/index',
+            name: 'index',
+            component: index
+        },
+        {
+            path: '/createWallet',
+            name: 'createWallet',
+            component: createWallet
+        },
+        {
+            path: '/importWallet',
+            name: 'importWallet',
+            component: importWallet
+        },
+        {
+            path: '/components',
+            name: 'header',
+            component: header
+        },
+        {
+            path: '/components',
+            name: 'footerBar',
+            component: footerBar
+        },
+        {
+            path: '/components',
+            name: 'pageHead',
+            component: pageHead
+        },
+        {
+            path: '/information',
+            name: 'information',
+            component: information
+        },
+        {
+            path: '/application',
+            name: 'application',
+            component: application
+        },
+        {
+            path: '/userCenter',
+            name: 'userCenter',
+            component: userCenter
+        },
+        {
+            path: '/transactionRelease',
+            name: 'transactionRelease',
+            component: transactionRelease
+        },
+        {
+            path: '/order',
+            name: 'order',
+            component: order
+        },
+        {
+            path: '/orderPage',
+            name: 'orderPage',
+            component: orderPage
+        },
+        {
+            path: '/forgetPassword',
+            name: 'forgetPassword',
+            component: forgetPassword
+        },
+        {
+            path: '/securityCenter',
+            name: 'securityCenter',
+            component: securityCenter
+        },
+        {
+            path: '/moreInformation',
+            name: 'moreInformation',
+            component: moreInformation
+        },
+        {
+            path: '/aboutUs',
+            name: 'aboutUs',
+            component: aboutUs
+        },
+        {
+            path: '/myTeam',
+            name: 'myTeam',
+            component: myTeam
+        },
+        {
+            path: '/personalData',
+            name: 'personalData',
+            component: personalData
+        },
+        {
+            path: '/financial',
+            name: 'financial',
+            component: financial
+        },
+        {
+            path: '/myInvestment',
+            name: 'myInvestment',
+            component: myInvestment
+        },
+        {
+            path: '/tenpay',
+            name: 'tenpay',
+            component: tenpay
+        },
+        {
+            path: '/walletDetails',
+            name: 'walletDetails',
+            component: walletDetails
+        },
+        {
+            path: '/walletTransfer',
+            name: 'walletTransfer',
+            component: walletTransfer
+        },
+        {
+            path: '/contacts',
+            name: 'contacts',
+            component: contacts
+        },
+        {
+            path: '/modifyContact',
+            name: 'modifyContact',
+            component: modifyContact
+        },
+        {
+            path: '/addContact',
+            name: 'addContact',
+            component: addContact
+        },
+        {
+            path: '/identity',
+            name: 'identity',
+            component: identity
+        },
+        {
+            path: '/paymentMethod',
+            name: 'paymentMethod',
+            component: paymentMethod
+        },
+        {
+            path: '/addPayment',
+            name: 'addPayment',
+            component: addPayment
+        },
+        {
+            path: '/transferRecord',
+            name: 'transferRecord',
+            component: transferRecord
+        },
+        {
+            path: '/appeal',
+            name: 'appeal',
+            component: appeal
+        }
+    ]
 })
